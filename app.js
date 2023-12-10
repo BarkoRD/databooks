313254a2eeb0b3e6af5a355f93429a68aa3a1584
nicolle <niicostars04@gmail.com>
2023-12-10 11:12:55 -0400

    Mensaje del commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // MANEJO DE SOLICITUDES GET
 
 // /^\/(index|crearClientes|crearProveedores)$/  -->  regex para index, crearClientes y crearProveedores
-app.get(['/', '/index', '/crearClientes', '/crearProveedores'], (req, res) => {
+app.get(['/', '/index', '/crearClientes', '/crearProveedores', '/crearProductos'], (req, res) => {
   req.session.loggedin ? res.render(req.url.slice(1)) : res.redirect('login')
 })
 
@@ -42,6 +42,18 @@ app.get(['/clientes', '/proveedores'], async (req, res) => {
   req.session.loggedin ? res.render(`${select}`, { tableitems }) : res.redirect('login')
   console.log(tableitems)
 })
+
+// GET PRODUCTOS
+
+app.get('/productos', async (req, res) => {
+  const selectOne = 'productos'
+  const id = req.session.user_id
+  const [tableitem] = await pool.promise().query(`SELECT * FROM ${selectOne} WHERE user_id = ?`, id)
+  req.session.loggedin ? res.render(`${selectOne}`, { tableitem }) : res.redirect('login')
+  console.log(tableitem)
+})
+
+// GET LOGIN
 
 app.get(['/login', '/register'], (req, res) => {
   req.session.loggedin ? res.redirect('index') : res.render(req.url.slice(1), { error: '' })
@@ -80,6 +92,8 @@ app.post('/login', async (req, res) => {
   }
 })
 
+// POST clientes y proveedores
+
 app.post(['/crearClientes', '/crearProveedores'], async (req, res) => {
   const newData = req.body
   const id = req.session.user_id
@@ -91,6 +105,42 @@ app.post(['/crearClientes', '/crearProveedores'], async (req, res) => {
   } else {
     await pool.promise().query(`INSERT INTO ${select} SET ?`, newData)
     res.redirect(`${select}`)
+  }
+})
+
+// POST PRODUCTOS
+
+app.post('/crearProductos', async (req, res) => {
+  const newData = {
+    product_id: req.product_id,
+    product_name: req.body.product_name,
+    reference: req.body.reference,
+    product_description: req.body.product_description,
+    cost: req.body.cost,
+    price: req.body.price,
+    supplier_rnc: req.body.supplier_rnc,
+    user_id: req.user_id
+  }
+
+  const id = req.session.user_id
+  newData.user_id = id
+
+  const selectOne = 'productos'
+  const [data] = await pool.promise().query(
+    `SELECT * FROM ${selectOne} WHERE supplier_rnc = ?`,
+    [newData.supplier_rnc]
+  )
+
+  if (data.length > 0) {
+    // El producto ya existe
+  } else {
+    // El producto no existe, se crea
+    res.render(req.url.slice(1))
+    await pool.promise().query(
+      `INSERT INTO ${selectOne} SET?`,
+      newData
+    )
+    res.redirect(`${selectOne}`)
   }
 })
 
